98074dcae8e60dc42fa4e2a7dad5444bff1c88a1
20191844323 <3080300351@qq.com>
2024-03-12 20:33:30 +0000

    Update

--- IT_Teamproject/__init__.py
+++ IT_Teamproject/__init__.py
@@ -1,3 +1,7 @@
-import pymysql
+# import pymysql
+#
+# pymysql.install_as_MySQLdb()
 
+import pymysql
+pymysql.version_info = (1, 4, 3, "final", 0)
 pymysql.install_as_MySQLdb()
--- IT_Teamproject/urls.py
+++ IT_Teamproject/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('portal.urls')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
